3c93dd45e820c2f22ab03d753e4cdb398ba309af
Saepudin <saep@ymail.com>
2025-09-26 10:54:42 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    rekabit.runMotor(MotorChannel.All, MotorDirection.Forward, 255)
-})
-input.onButtonPressed(Button.B, function () {
-    rekabit.brakeMotor(MotorChannel.All)
-})
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . # . .
-    # . . . #
-    . # # # .
-    `)
+
